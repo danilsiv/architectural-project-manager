a96752e72609cc5fe7c5474e63287dee8e1db113
Sivkovich Danylo <danilsivkovic@gmail.com>
2024-11-12 22:40:09 +0200

    Add registration for Worker in admin panel

--- management/admin.py
+++ management/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-from management.models import Position, ProjectType, Project
+from management.models import Worker, Position, ProjectType, Project
 
 
 @admin.register(Position)
@@ -13,3 +14,31 @@ class PositionAdmin(admin.ModelAdmin):
 class ProjectTypeAdmin(admin.ModelAdmin):
     search_fields = ("name", )
     ordering = ("name", )
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = (
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "position",
+    )
+    ordering = ("position", )
+    search_fields = (
+        "username",
+        "first_name",
+        "last_name",
+        "position__name"
+    )
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional information", {
+            "fields": ("position", ),
+        }),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional information", {
+            "fields": ("first_name", "last_name", "email", "position"),
+        }),
+    )
