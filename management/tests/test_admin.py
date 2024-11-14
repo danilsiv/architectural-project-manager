3d106f90642cedd6186de86932731ba0601e5fdc
Sivkovich Danylo <danilsivkovic@gmail.com>
2024-11-14 08:36:24 +0200

    Add tests for ProjectType model in admin panel

--- management/tests/test_admin.py
+++ management/tests/test_admin.py
@@ -2,7 +2,7 @@ from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 
-from management.models import Position
+from management.models import Position, ProjectType
 
 
 class AdminPositionTests(TestCase):
@@ -21,5 +21,26 @@ class AdminPositionTests(TestCase):
         :return:
         """
         url = reverse("admin:management_position_changelist")
+        response = self.client.get(url)
+        self.assertContains(response, "name")
+
+
+class AdminProjectTypeTests(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            username="test_admin",
+            password="test123admin"
+        )
+        self.client.force_login(self.admin_user)
+        self.project_type = ProjectType.objects.create(name="test_project_type")
+
+    def test_project_type_name_should_be_in_search_field(self) -> None:
+        """
+        Test that name of project type is in search_field on
+        list page of project types
+        :return:
+        """
+        url = reverse("admin:management_projecttype_changelist")
         response = self.client.get(url)
         self.assertContains(response, "name")
