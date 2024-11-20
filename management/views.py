659fcb98bb15bfc1a1934dc6dba0bc1a194b5ce8
Sivkovich Danylo <danilsivkovic@gmail.com>
2024-11-20 20:07:14 +0200

    Initialize index.html with context placeholders

--- management/views.py
+++ management/views.py
@@ -7,16 +7,12 @@ from management.models import (
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    num_project_types = ProjectType.objects.count()
     num_teams = Team.objects.count()
     num_workers = Worker.objects.count()
-    num_positions = Position.objects.count()
     num_projects = Project.objects.count()
     context = {
-        "num_project_types": num_project_types,
         "num_teams": num_teams,
         "num_workers": num_workers,
-        "num_positions": num_positions,
         "num_projects": num_projects,
     }
     return render(
