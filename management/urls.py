b4d878aa9fb69188a1f009a1a5c70bbc1a1a7292
Sivkovich Danylo <danilsivkovic@gmail.com>
2024-11-20 20:50:12 +0200

    Views: Implement ProjectTypeListView with URL mapping and basic template

--- management/urls.py
+++ management/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from management.views import index
+from management.views import (
+    index,
+    ProjectTypeListView,
+)
 
 
 urlpatterns = [
     path("", index, name="index"),
+    path("project-types/", ProjectTypeListView.as_view(), name="project-type-list"),
 ]
 
 app_name = "management"
--- management/views.py
+++ management/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views import generic
 
 from management.models import (
     ProjectType, Team, Worker, Position, Project
@@ -18,3 +19,9 @@ def index(request: HttpRequest) -> HttpResponse:
     return render(
         request, "management/index.html", context=context
     )
+
+
+class ProjectTypeListView(generic.ListView):
+    model = ProjectType
+    template_name = "management/project_type_list.html"
+    context_object_name = "project_type_list"
