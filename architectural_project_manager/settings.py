d3e706afe52663167e90b808bfed52bb74997caf
Sivkovich Danylo <danilsivkovic@gmail.com>
2024-11-23 17:42:29 +0200

    Integrate frontend settings, static files and initial templates

--- architectural_project_manager/settings.py
+++ architectural_project_manager/settings.py
@@ -67,6 +67,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "management.context_processors.cfg_assets_root",
             ],
         },
     },
@@ -125,6 +126,8 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static/",
 ]
+
+ASSETS_ROOT = "/static/assets/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
