55223313028e943a51cdff41b8728eec01d74230
Sivkovich Danylo <danilsivkovic@gmail.com>
2024-11-28 08:50:27 +0200

    Forms: Create Worker form for managing Worker data

---
+++ management/forms.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.forms import UserCreationForm
+
+from management.models import Worker
+
+
+class WorkerCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Worker
+        fields = UserCreationForm.Meta.fields + (
+            "first_name",
+            "last_name",
+            "email",
+            "position",
+            "team"
+        )
