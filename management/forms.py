a3f492f50caab8ccafce04480b04265e091b848e
Sivkovich Danylo <danilsivkovic@gmail.com>
2024-12-02 14:31:38 +0200

    Forms: Create TeamCreationForm

--- management/forms.py
+++ management/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
 
-from management.models import Worker
+from management.models import Worker, Team, Project
 
 
 class WorkerCreationForm(UserCreationForm):
@@ -13,3 +14,27 @@ class WorkerCreationForm(UserCreationForm):
             "position",
             "team"
         )
+
+
+class TeamCreationForm(forms.ModelForm):
+    members = forms.ModelMultipleChoiceField(
+        queryset=Worker.objects.filter(team=None).exclude(
+            position__name="admin").select_related("position"),
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+        label="Select Team Members"
+    )
+    projects = forms.ModelMultipleChoiceField(
+        queryset=Project.objects.filter(team=None),
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+        label="Assign Projects"
+    )
+    team_lead = forms.ModelChoiceField(
+        queryset=Worker.objects.filter(team=None).exclude(
+            position__name="admin").select_related("position")
+    )
+
+    class Meta:
+        model = Team
+        fields = ("name", "team_lead", "members", "projects")
