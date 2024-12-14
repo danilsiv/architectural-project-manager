043bd7394d4f808a4dfe4a1f883b84b0638ccf67
Sivkovich Danylo <danilsivkovic@gmail.com>
2024-12-14 04:24:01 +0200

    Tests forms:  Implement tests for TeamCreationForm

--- management/tests/test_forms.py
+++ management/tests/test_forms.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 
-from management.forms import WorkerCreationForm
-from management.models import Worker, Position, Team
+from management.forms import WorkerCreationForm, TeamCreationForm
+from management.models import Worker, Position, Team, Project, ProjectType
 
 
 class WorkerCreationFormTests(TestCase):
@@ -98,3 +98,103 @@ class WorkerCreationFormTests(TestCase):
         self.assertFalse(form.is_valid())
         self.assertIn("position", form.errors)
         self.assertIn("team", form.errors)
+
+
+class TeamCreationFormTests(TestCase):
+    def create_worker(self, username: str) -> Worker:
+        return Worker.objects.create_user(
+            username=username, password="test123user", position=self.position
+        )
+
+    def create_project(self, name: str) -> Project:
+        return Project.objects.create(
+            name=name,
+            description="test_description",
+            deadline="2027-12-12",
+            project_type=self.project_type
+        )
+
+    def setUp(self) -> None:
+        self.position = Position.objects.create(name="test_position")
+        self.project_type = ProjectType.objects.create(name="test_project_type")
+        self.team_lead = self.create_worker("test_team_lead")
+        self.worker_1 = self.create_worker("first_worker")
+        self.worker_2 = self.create_worker("second_worker")
+        self.project_1 = self.create_project("first_project")
+        self.project_2 = self.create_project("second_project")
+
+        self.form_data = {
+            "name": "test_team",
+            "team_lead": self.team_lead.id,
+            "members": [worker.id for worker in Worker.objects.all()],
+            "projects": [project.id for project in Project.objects.all()]
+        }
+
+    def test_creation_form_data_is_valid(self) -> None:
+        form = TeamCreationForm(data=self.form_data)
+        self.form_data["team_lead"] = Worker.objects.get(id=self.form_data["team_lead"])
+        self.form_data["members"] = Worker.objects.all()
+        self.form_data["projects"] = Project.objects.all()
+
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.cleaned_data["name"], self.form_data["name"])
+        self.assertEqual(form.cleaned_data["team_lead"], self.form_data["team_lead"])
+        self.assertEqual(list(form.cleaned_data["members"]), list(self.form_data["members"]))
+        self.assertEqual(list(form.cleaned_data["projects"]), list(self.form_data["projects"]))
+
+    def test_empty_optional_fields(self) -> None:
+        self.form_data["members"] = []
+        self.form_data["projects"] = []
+
+        form = TeamCreationForm(data=self.form_data)
+
+        self.assertTrue(form.is_valid())
+
+    def test_form_invalid_when_required_fields_missing(self) -> None:
+        self.form_data.pop("name")
+        self.form_data.pop("team_lead")
+
+        form = TeamCreationForm(data=self.form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn("name", form.errors)
+        self.assertIn("team_lead", form.errors)
+
+    def test_form_invalid_with_nonexistent_related_object(self) -> None:
+        self.form_data["team_lead"] = 9999
+        self.form_data["members"] = [9999, 9999]
+        self.form_data["projects"] = [9999, 9999]
+
+        form = TeamCreationForm(data=self.form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn("team_lead", form.errors)
+        self.assertIn("members", form.errors)
+        self.assertIn("projects", form.errors)
+
+    def test_team_lead_and_workers_queryset_excludes_admin_and_assigned_workers(self) -> None:
+        worker_admin = self.create_worker("worker_admin")
+        worker_admin.position = Position.objects.create(name="admin")
+        worker_admin.save()
+
+        assigned_worker = self.worker_1
+        unassigned_worker = self.worker_2
+
+        team_lead_queryset = TeamCreationForm().fields["team_lead"].queryset
+        workers_queryset = TeamCreationForm().fields["members"].queryset
+        team = Team.objects.create(name="test_team", team_lead=worker_admin)
+        team.members.set([assigned_worker])
+
+        self.assertNotIn(assigned_worker, team_lead_queryset)
+        self.assertNotIn(worker_admin, team_lead_queryset)
+        self.assertIn(unassigned_worker, team_lead_queryset)
+        self.assertEqual(list(team_lead_queryset), list(workers_queryset))
+
+    def test_projects_queryset_excludes_assigned_projects(self) -> None:
+        assigned_project = self.project_1
+        unassigned_project = self.project_2
+
+        projects_queryset = TeamCreationForm().fields["projects"].queryset
+        team = Team.objects.create(name="test_team", team_lead=self.team_lead)
+        team.projects.set([assigned_project])
+
+        self.assertNotIn(assigned_project, projects_queryset)
+        self.assertIn(unassigned_project, projects_queryset)
